4b82b464aa980c697250c08a542a980532020f07
a1b0r <a1b0@pm.me>
2022-11-21 03:27:38 +0100

    Create test.php
    insomnia paste

---
+++ test.php
@@ -0,0 +1,116 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+    CURLOPT_PORT => "8000",
+    CURLOPT_URL => "http://127.0.0.1:8000/api/addressbook?=",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => "{\"id\":\"1\",\"name\":\"asdfasdf\",\"openingHours\":\"public/public/\",\"telephone\":\"\",\"country\":\"public/\",\"locality\":\"\",\"region\":\"\",\"code\":\"\",\"streetAddress\":\"\"}",
+    CURLOPT_HTTPHEADER => [
+      "Content-Type: application/json"
+    ],
+  ]);
+  
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+  
+  curl_close($curl);
+  echo 'create ';
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    echo $response;
+  }
+  echo PHP_EOL;
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_PORT => "8000",
+  CURLOPT_URL => "http://127.0.0.1:8000/api/addressbook",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "PUT",
+  CURLOPT_POSTFIELDS => "{\"id\":\"1\",\"name\":\"asdfasdf\",\"openingHours\":\"public/public/\",\"telephone\":\"\",\"country\":\"public/\",\"locality\":\"\",\"region\":\"\",\"code\":\"\",\"streetAddress\":\"\"}",
+  CURLOPT_HTTPHEADER => [
+    "Content-Type: application/json"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+echo 'update ';
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+echo PHP_EOL;
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_PORT => "8000",
+  CURLOPT_URL => "http://127.0.0.1:8000/api/addressbook",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_POSTFIELDS => "",
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+echo 'read ';
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+echo PHP_EOL;
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_PORT => "8000",
+  CURLOPT_URL => "http://127.0.0.1:8000/api/addressbook",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "DELETE",
+  CURLOPT_POSTFIELDS => "{\"id\":1}",
+  CURLOPT_HTTPHEADER => [
+    "Content-Type: application/json"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+echo 'delete ';
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+echo PHP_EOL;
